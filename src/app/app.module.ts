de6e57f141c942c8166fcff70797ff9b0705fe72
Tigerking <tigerking@tigerking.localdomain>
2017-11-15 15:40:31 +0530

    Material UI integrated for the project : Final Tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+// MODULES USED
+
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule, MatMenuModule } from '@angular/material';
+import 'hammerjs';
 
-
+// COMPONENTS USED
 import { AppComponent } from './app.component';
 
+// INJECTIONS
+const MODULES = [ BrowserModule, BrowserAnimationsModule , MatButtonModule, MatCheckboxModule, MatMenuModule ];
+const COMPONENTS = [AppComponent];
+const SINGLETONSERVICES = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [ ...COMPONENTS ],
+  imports: [ ...MODULES ],
+  providers: [ ...SINGLETONSERVICES ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
