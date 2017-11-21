0b0202c7742d6f52e1a4637b307afe788bae02ed
Ankur <ankur.gupta@sugaldamani.com>
2017-11-21 16:01:46 +0530

    Fixed login component - removed alert()

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,7 +24,6 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     // jquery setup for form
     $('.toggle').on('click', function() {
-      alert('dddd');
       $('.container').stop().addClass('active');
     });
 
