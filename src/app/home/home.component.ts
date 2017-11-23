e30839fbb3ef8fc2663e11efc1ddd84da0f107fb
Madhu <madhu.gupta@sugaldamani.com>
2017-11-23 18:30:46 +0530

    landing page
    as per marketting new landing page is added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,14 @@ import { UserService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'home.component.html'
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
+    headertype ="public";
 
     constructor(private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
