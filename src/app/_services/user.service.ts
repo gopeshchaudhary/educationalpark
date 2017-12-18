80050b33aa3d5fe6699953e01d022ca62f825479
Gopesh Chaudhary <gopesh.chaudhary@sugaldamani.com>
2017-12-18 13:38:50 +0530

    Forget Password functionality added

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,12 +1,13 @@
-﻿import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+﻿import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
 
-import { User } from '../_models/index';
-import { error } from 'selenium-webdriver';
+import {User} from '../_models/index';
+import {error} from 'selenium-webdriver';
 
 @Injectable()
 export class UserService {
-    constructor(private http: Http) { }
+    constructor(private http: Http) {
+    }
 
     getAll() {
         return this.http.get('/users').map((response: Response) => response.json());
@@ -29,7 +30,7 @@ export class UserService {
     }
 
     getprofile(username) {
-        return this.http.post('/profile/get', { username })
+        return this.http.post('/profile/get', {username})
             .map((response: Response) => {
                 // getprofile successful if there's userprofile in response
                 let profile = response.json();
@@ -41,7 +42,7 @@ export class UserService {
     }
 
     resetPassword(username, oldPassword, newPassword) {
-        return this.http.post('/profile/update', { username, oldPassword, newPassword })
+        return this.http.post('/profile/update', {username, oldPassword, newPassword})
             .map((response: Response) => {
                 // resetPassword successful if password got changed in response
                 let res = response.json();
@@ -49,8 +50,17 @@ export class UserService {
             });
     }
 
+    forgetPassword(username, emailID) {
+        return this.http.post('/auth/reset', {username, emailID})
+            .map((response: Response) => {
+                // forgetPassword successful if password got reset in response
+                let res = response.json();
+                return res;
+            });
+    }
+
     generateOTP(username, mobileno) {
-        return this.http.post('/auth/generate', { username, mobileno })
+        return this.http.post('/auth/generate', {username, mobileno})
             .map((response: Response) => {
                 // generateOTP successful if otp is sent to mobile as response
                 let gen = response.json();
@@ -59,7 +69,7 @@ export class UserService {
     }
 
     verifyOTP(username, otp) {
-        return this.http.post('/auth/verify', { username, otp })
+        return this.http.post('/auth/verify', {username, otp})
             .map((response: Response) => {
                 // generateOTP successful if otp is sent to mobile as response
                 let verify = response.json();
@@ -68,13 +78,13 @@ export class UserService {
     }
 
     sendMail(username, mobileno, email) {
-        return this.http.post('/auth/sendmail', { username, mobileno, email })
+        return this.http.post('/auth/sendmail', {username, mobileno, email})
             .map((response: Response) => {
                 let mail = response.json();
                 return mail;
             });
     }
-    
+
     // get username for getting profile
     getUsername() {
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,49 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 
-import { AlertService, AuthenticationService, UserService } from '../_services/index'; 
+import {AlertService, AuthenticationService, UserService} from '../_services/index';
 
 @Component({
-  selector: 'app-forget-password',
-  templateUrl: './forget-password.component.html',
-  styleUrls: ['./forget-password.component.css']
+    selector: 'app-forget-password',
+    templateUrl: './forget-password.component.html',
+    styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent implements OnInit {
-  
-  model: any = {};
-  loading:boolean;
-  firstStepPassword : boolean;
-  secondStepPassword : boolean;
-  thirdStepPassword : boolean;
-  thankumessage : boolean;
-  headertype = "public";
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private userService: UserService,
-    private alertService: AlertService
-  ) { }
+    model: any = {};
+    loading: boolean;
+    firstStepPassword: boolean;
+    secondStepPassword: boolean;
+    thirdStepPassword: boolean;
+    thankumessage: boolean;
+    headertype = "public";
 
-  ngOnInit() {
-    this.loading = false;
-    this.firstStepPassword = true;
-    this.secondStepPassword = false;
-    this.thirdStepPassword = false;
-    this.thankumessage = false;
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private authenticationService: AuthenticationService,
+                private userService: UserService,
+                private alertService: AlertService) {
+    }
 
-  }
+    ngOnInit() {
+        this.loading = false;
+        this.firstStepPassword = true;
+        this.secondStepPassword = false;
+        this.thirdStepPassword = false;
+        this.thankumessage = false;
 
-  firstStepPasswordFunc(){
-    
-  }
+    }
 
-  secondStepPasswordFunc(){
+    forgetPassword() {
+        this.loading = true;
+        console.log(this.model);
+        this.userService.forgetPassword(this.model.loginname, this.model.email)
+            .subscribe(
+                response => {
+                    if (response.reset == "success") {
+                        this.alertService.success("You have succesfully reset the password",true);
+                        //this.mobileOtpVerify = false;
+                        this.firstStepPassword = false;
+                        this.thankumessage = true;
+                    }
+                    else {
+                        this.alertService.error("There is some error..Please Try Again");
+                    }
+                },
+                error => {
+                    this.alertService.error(error);
+                });
+    }
 
-  }
+    firstStepPasswordFunc() {
 
-  thirdStepPasswordFunc(){
+    }
 
-  }
+    secondStepPasswordFunc() {
+
+    }
+
+    thirdStepPasswordFunc() {
+
+    }
 }
